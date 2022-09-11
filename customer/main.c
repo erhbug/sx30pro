b929999b784d40533bf6c2727f1e2db55703c16e
fernandomatag <fernando.mata@alpuntofs.com>
2022-09-10 19:48:08 -0500

    adc para volataje bateria
    Lectura de voltatje en P0.5/AD1 funcionando

--- customer/main.c
+++ customer/main.c
@@ -16,7 +16,7 @@ unsigned char lecturaADC[4]= {0};
 #define     BEEPER_EN	P1 |= 0x10
 #define     BEEPER_DIS	P1 &= 0xEF
 
-
+unsigned int convertidorADC(void);
 
 void init_pwm(void){
     PWMF_H  = 0x00;
@@ -26,11 +26,9 @@ void init_pwm(void){
 	PWMCON  = 0x04;	//PWM0-P1.4(LCD_LAMP)????(?PWM0=0xff?,?????)
 }
 
-unsigned char a[512]={0};
-
 void main(void)
 {char txt[5]={0};
- float peso=0;
+ float peso=0, voltaje=0;
 	//int a = 0, b = 0;
     P0M0 = 0xF0; //0b11111111;
     P0M1 = 0x00; //0b00000000;    
@@ -38,6 +36,9 @@ void main(void)
     P1M1 = 0x00; //0b00000000;        
     P2M0 = 0xEE; //0b11101110;
     P2M1 = 0x00; //0b00000000;
+	//Config lectura adc
+	P0M0 &= ~(1<<5);
+	P0M1 |= (1<<5);
 /*bit KEY_K0 = P0^2;	
 sbit KEY_K1 = P2^4;	
 sbit KEY_K2 = P2^0;	
@@ -51,7 +52,9 @@ sbit KEY_K3 = P1^6;		*/
     P0 = 0x0C;//P0 = 0x04;
     P1 = 0x40;
     P2 = 0x11;
-
+	//Salida en 1  para lectura adc
+	P0|= (1<<5);
+	
 /*	P2 = 0x00;
 	P1 = 0x00;*/
 
@@ -59,7 +62,7 @@ sbit KEY_K3 = P1^6;		*/
 			delay_ms(500);
 	LCD_GLASS_String("TORR",LCD_PESO);
 		delay_ms(500);
-	LCD_GLASS_String("EY-",LCD_PRECIO);
+	LCD_GLASS_String("EY",LCD_PRECIO);
 			delay_ms(500);    
  
 	init_pwm();
@@ -84,7 +87,7 @@ KEY_D4=0;
 delay_ms(100);
 
 	}*/
-
+ iLCD_GLASS_Clear();
  while(1){   
  /*key_scan();
 	sprintf(txt,"%d  ",(int)(Key));
@@ -93,13 +96,32 @@ delay_ms(100);
 	//sprintf(txt,"%d  ",(int)(KeyState));
 	//LCD_GLASS_String(txt,LCD_PRECIO);
 
-
+	
 	delay_ms(50);
 
 	peso=fRead_Adc(0);
+	P0|= (1<<5);
+	voltaje=convertidorADC()*(3.3/255);
 	LCD_GLASS_Float(peso, 2,  LCD_TOTAL);
-
+	LCD_GLASS_Float(voltaje, 2, LCD_PESO);
 
 }
+
+}
+
+unsigned int convertidorADC(){
+
+unsigned int v=0;
+SARCON = 0x09;
+	if(!(SARCON & 0x04))
+	{
+		SARCON |= 0x04;
+		while(SARCON & 0x04)
+		{
+		}
+	}
+v=SARDATA;
+SARCON &= 0xf7;
+return v;
 
 }
